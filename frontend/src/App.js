d9027a509e91cdd61bfee9a8d612c95242338d05
hansen1416 <badapplesweetie@gmail.com>
2022-09-25 14:46:55 +0800

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,108 +1,25 @@
-import "./App.css";
-
-import React, { useEffect, useRef, useState } from "react";
-import { Pose } from "@mediapipe/pose";
-
-// Integrate navigator.getUserMedia & navigator.mediaDevices.getUserMedia
-function getUserMedia(constraints, successCallback, errorCallback) {
-	if (!constraints || !successCallback || !errorCallback) {
-		return;
-	}
-
-	if (navigator.mediaDevices) {
-		navigator.mediaDevices
-			.getUserMedia(constraints)
-			.then(successCallback, errorCallback);
-	} else {
-		navigator.getUserMedia(constraints, successCallback, errorCallback);
-	}
-}
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-	useEffect(() => {
-		let camera_button = document.querySelector("#start-camera");
-		let video = document.querySelector("#video");
-		// let start_button = document.querySelector("#start-record");
-		// let stop_button = document.querySelector("#stop-record");
-		// let download_link = document.querySelector("#download-video");
-
-		let camera_stream = null;
-		let media_recorder = null;
-		let blobs_recorded = [];
-
-		const canvas = document.querySelector("#output_canvas");
-		const ctx = canvas.getContext("2d");
-
-		function onResults(results) {
-			console.log(results);
-		}
-
-		const pose = new Pose({
-			locateFile: (file) => {
-				return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
-			},
-		});
-
-		pose.setOptions({
-			modelComplexity: 1,
-			smoothLandmarks: true,
-			enableSegmentation: true,
-			smoothSegmentation: true,
-			minDetectionConfidence: 0.5,
-			minTrackingConfidence: 0.5,
-		});
-		pose.onResults(onResults);
-
-		camera_button.addEventListener("click", () => {
-			getUserMedia(
-				{ video: true },
-				function (stream) {
-					// Yay, now our webcam input is treated as a normal video and
-					// we can start having fun
-					try {
-						video.srcObject = stream;
-					} catch (error) {
-						video.src = URL.createObjectURL(stream);
-					}
-					// Let's start drawing the canvas!
-					draw_loop();
-				},
-				function (err) {
-					throw err;
-				}
-			);
-		});
-
-		function draw_loop() {
-			ctx.drawImage(video, 0, 0);
-
-			// console.log(canvas.toDataURL());
-
-			// pose.send({ image: canvas.toDataURL() }).then((r) => {
-			// 	console.log(1111, r);
-			// });
-
-			requestAnimationFrame(draw_loop);
-		}
-	}, []);
-
-	return (
-		<div className="App">
-			<header className="App-header">
-				<div>
-					<div>home</div>
-
-					<button id="start-camera">Start Camera</button>
-					<video id="video" autoplay="true"></video>
-					<canvas
-						id="output_canvas"
-						width="640px"
-						height="360px"
-					></canvas>
-				</div>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,17 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-import "./index.css";
-// import App from "./App";
-import RouterConfig from "./router";
-import dva from "dva";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const app = dva({
-	// history: createHistory(),
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// 4. Router
-app.router(RouterConfig);
-
-// 5. Start
-app.start("#root");
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
