3d8276263772538ff019149d17cd1d8ee650cf9c
Hanson <105476@grundfos.com>
2022-10-17 15:48:18 +0800

    1

--- frontend/src/components/Scene.js
+++ frontend/src/components/Scene.js
@@ -2,14 +2,149 @@ import { useEffect, useRef } from "react";
 import "./Home.css";
 
 import * as THREE from "three";
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+// import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+
+const degreesToRadians = (degrees) => {
+	return degrees * (Math.PI / 180)
+}
+
+class Figure {
+	constructor(scene, params) {
+		
+		console.log(scene)
+		
+		
+		this.params = {
+			x: 0,
+			y: 0,
+			z: 0,
+			ry: 0,
+			...params
+		}
+		
+		this.material = new THREE.MeshLambertMaterial({ color: 0xffffff })
+
+		this.group = new THREE.Group()
+		
+		this.group.position.x = this.params.x
+		this.group.position.y = this.params.y
+		this.group.position.z = this.params.z
+		this.group.position.ry = this.params.ry
+
+		scene.add(this.group)
+	}
+
+	createBody() {
+		const geometry = new THREE.BoxGeometry(1, 1.5, 1)
+		const body = new THREE.Mesh(geometry, this.material)
+		this.group.add(body)
+	}
+
+	createHead() {
+		// Create a new group for the head
+		this.head = new THREE.Group()
+		
+		// Create the main cube of the head and add to the group
+		const geometry = new THREE.BoxGeometry(1.4, 1.4, 1.4)
+		const headMain = new THREE.Mesh(geometry, material)
+		this.head.add(headMain)
+		
+		// Add the head group to the figure
+		this.group.add(this.head)
+		
+		// Position the head group
+		this.head.position.y = 1.65
+		
+		// Add the eyes by calling the function we already made
+		this.createEyes()
+	}
+
+	createArms() {
+		// Set the variable
+		const height = 1
+		const geometry = new THREE.BoxGeometry(0.25, height, 0.25)
+		
+		for(let i = 0; i < 2; i++) {
+			const armGroup = new THREE.Group()
+			const arm = new THREE.Mesh(geometry, this.material)
+			
+			const m = i % 2 === 0 ? 1 : -1
+			
+			armGroup.add(arm)
+			this.group.add(armGroup)
+			
+			// Translate the arm (not the group) downwards by half the height
+			arm.position.y = height * -0.5
+			
+			armGroup.position.x = m * 0.8
+			armGroup.position.y = 0.6
+
+			armGroup.rotation.z = degreesToRadians(30 * m)
+			
+			// Helper
+			const box = new THREE.BoxHelper(armGroup, 0xffff00)
+			this.group.add(box)
+		}
+	}
+
+	createEyes() {
+		const eyes = new THREE.Group()
+		const geometry = new THREE.SphereGeometry(0.15, 12, 8)
+		
+		// Define the eye material
+		const material = new THREE.MeshLambertMaterial({ color: 0x44445c })
+		
+		for(let i = 0; i < 2; i++) {
+			const eye = new THREE.Mesh(geometry, material)
+			const m = i % 2 === 0 ? 1 : -1
+			
+			// Add the eye to the group
+			eyes.add(eye)
+			
+			// Position the eye
+			eye.position.x = 0.36 * m
+		}
+
+		// in createEyes()
+		this.head.add(eyes)
+
+		// Move the eyes forwards by half of the head depth - it might be a good idea to create a variable to do this!
+		eyes.position.z = 0.7
+	}
+
+	createLegs() {
+		const legs = new THREE.Group()
+		const geometry = new THREE.BoxGeometry(0.25, 0.4, 0.25)
+		
+		for(let i = 0; i < 2; i++) {
+			const leg = new THREE.Mesh(geometry, material)
+			const m = i % 2 === 0 ? 1 : -1
+			
+			legs.add(leg)
+			leg.position.x = m * 0.22
+		}
+		
+		this.group.add(legs)
+		legs.position.y = -1.15
+		
+		this.body.add(legs)
+	}
+
+	init() {
+		this.createBody()
+		this.createHead()
+		this.createArms()
+		this.createLegs()
+	}
+}
+
 
 export default function Scene() {
+
+	const canvasRef = useRef(null);
 	const scene = useRef(null);
 	const camera = useRef(null);
 	const renderer = useRef(null);
-
-	const animationframe = useRef(0);
 
 	useEffect(() => {
 		scene.current = new THREE.Scene();
@@ -37,49 +172,39 @@ export default function Scene() {
 			1000
 		);
 
-		renderer.current = new THREE.WebGLRenderer();
+		camera.current.position.z = 5;
+
+		scene.current.add(camera.current)
+
+		renderer.current = new THREE.WebGLRenderer({canvas: canvasRef.current});
 		renderer.current.setSize(window.innerWidth / 2, window.innerHeight / 2);
-		document.body.appendChild(renderer.current.domElement);
 
-		const loader = new GLTFLoader();
+		let fig = new Figure(scene.current)
+		fig.init()
 
-		loader.load(
-			process.env.PUBLIC_URL + `/models/Soldier.glb`,
-			function (gltf) {
-				scene.current.add(gltf.scene);
+		// direct light
+		const lightDirectional = new THREE.DirectionalLight(0xffffff, 1)
 
-				console.log(gltf);
-			},
-			undefined,
-			function (error) {
-				console.error(error);
-			}
-		);
+		scene.current.add(lightDirectional)
 
-		// const geometry = new THREE.BoxGeometry(1, 1, 1);
-		// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-		// const cube = new THREE.Mesh(geometry, material);
-		// scene.current.add(cube);
+		lightDirectional.position.set(5,5,5)
 
-		// camera.current.position.z = 5;
+		// env light
+		const lightAmbient = new THREE.AmbientLight(0x9eaeff, 0.2)
+		scene.current.add(lightAmbient)
 
-		// function animate() {
-		// 	animationframe.current = requestAnimationFrame(animate);
+		
 
-		// 	cube.rotation.x += 0.01;
-		// 	cube.rotation.y += 0.01;
-
-		// 	renderer.current.render(scene.current, camera.current);
-		// }
-
-		// animate();
+		renderer.current.render(scene.current, camera.current);
 
 		return () => {
 			// cancelAnimationFrame(animationframe.current);
-			// renderer.current.dispose();
+			renderer.current.dispose();
 			// document.body.removeChild(renderer.current.domElement);
 		};
 	}, []);
 
-	return <div></div>;
+	return (<div>
+		<canvas ref={canvasRef}></canvas>
+	</div>);
 }
