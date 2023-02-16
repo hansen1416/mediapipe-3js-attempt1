50c50d4db1dc66d6bcb0ab928cf3bc4ae2150b5a
hansen1416 <badapplesweetie@gmail.com>
2023-02-16 22:40:19 +0800

    1

--- flaskapi/config.py
+++ flaskapi/config.py
@@ -15,15 +15,15 @@ app.config['MAX_CONTENT_LENGTH'] = 80 * 1024 * 1024
 # used by jwt
 app.config['SECRET_KEY'] = SECRET_KEY
 # postgresql connection
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{user}:{pwd}@{host}:{port}/{dbname}".format(user=os.environ.get('DB_USERNAME'), 
-                                                                                                  pwd=os.environ.get('DB_PASSWORD'), 
-                                                                                                  host=os.environ.get('DB_HOST'),
-                                                                                                  port=os.environ.get('DB_PORT'), 
-                                                                                                  dbname=os.environ.get('DB_NAME'))
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://{user}:{pwd}@{host}:{port}/{dbname}"\
+    .format(user=os.environ.get('DB_USERNAME'),
+            pwd=os.environ.get('DB_PASSWORD'),
+            host=os.environ.get('DB_HOST'),
+            port=os.environ.get('DB_PORT'),
+            dbname=os.environ.get('DB_NAME'))
 
 # db migration
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 
 
 db = SQLAlchemy(app)
--- flaskapi/models.py
+++ flaskapi/models.py
@@ -10,8 +10,9 @@ class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(128))
     password = db.Column(db.String(255))
-    publickey=db.Column(db.String(128))
-    nonce=db.Column(db.String(128))
+    publickey = db.Column(db.String(128))
+    nonce = db.Column(db.String(128))
+
 
 class Profile(db.Model):
     """
@@ -20,12 +21,14 @@ class Profile(db.Model):
     """
     user_id = db.Column(db.Integer, primary_key=True)
 
+
 class Exercise(db.Model):
     """
     exercise info
     exercise_id, animation_file_s3_key
     """
     exercise_id = db.Column(db.Integer, primary_key=True)
+
 
 class Training(db.Model):
     """
@@ -34,6 +37,7 @@ class Training(db.Model):
     """
     training_id = db.Column(db.Integer, primary_key=True)
 
+
 class TrainingExercise(db.Model):
     """
     exercises in the training
@@ -41,6 +45,7 @@ class TrainingExercise(db.Model):
     """
     training_id = db.Column(db.Integer, primary_key=True)
     exercise_id = db.Column(db.Integer, primary_key=True)
+
 
 class TrainingHistory(db.Model):
     """
