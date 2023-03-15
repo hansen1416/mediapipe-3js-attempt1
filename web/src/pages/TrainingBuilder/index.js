19e679cc8f64ccfdee6056b25f050d5ab496703b
Hanson <105476@grundfos.com>
2023-03-15 13:29:33 +0800

    1

--- web/src/pages/TrainingBuilder/index.js
+++ web/src/pages/TrainingBuilder/index.js
@@ -1,21 +1,25 @@
 import { useEffect, useRef, useState } from "react";
 
+import TrainingSlideEditor from "../../components/TrainingSlideEditor";
 import MusclePercentage from "../../components/MusclePercentage";
+import "../../styles/css/TrainingBuilder.css"
 
 export default function TrainingBuilder() {
     const canvasRef = useRef(null);
 
     const kasten = useRef(null);
 
-    const [itemWidth, setitemWidth] = useState(0)
-    const [itemHeight, setitemHeight] = useState(0)
+    const [itemWidth, setitemWidth] = useState(0);
+    const [itemHeight, setitemHeight] = useState(0);
 
     const pageSize = 12;
 
-    const [totalPage] = useState([1,2,3])
-    const [currentPage, setcurrentPage] = useState(1)
-    const [allData, setallData] = useState([])
-    const [pageData, setpageData] =  useState([])
+    const [totalPage] = useState([1,2,3]);
+    const [currentPage, setcurrentPage] = useState(1);
+    const [allData, setallData] = useState([]);
+    const [pageData, setpageData] = useState([]);
+
+    const [trainingData, settrainingData] = useState([]);
 
 	useEffect(() => {
 
@@ -27,8 +31,8 @@ export default function TrainingBuilder() {
                 // Do what you want to do when the size of the element changes
                 const width = parseInt(ResizeObserverEntry.contentRect.width / 4);
 
-                setitemWidth(width)
-                setitemHeight(width + 100)
+                setitemWidth(width);
+                setitemHeight(width + 100);
             });
             resizeObserver.observe(kasten.current);
         }
@@ -59,6 +63,14 @@ export default function TrainingBuilder() {
 
 	}, []);
 
+    useEffect(() => {
+
+        if (!pageData || pageData.length === 0) {
+            loadPageData(1);
+        }
+
+    }, [allData]);
+
     function loadPageData(p) {
 
         setcurrentPage(p)
@@ -80,7 +92,13 @@ export default function TrainingBuilder() {
 			<div className="title">
 				<h1>Training Builder</h1>
 			</div>
-            <div className="title">
+            <div>
+                <TrainingSlideEditor
+                    trainingData={trainingData}
+                    settrainingData={settrainingData}
+                />
+            </div>
+            <div className="filters">
                 <div><span>Filter placeholder</span></div>
             </div>
 			<div>
@@ -89,10 +107,10 @@ export default function TrainingBuilder() {
                         return (
                                 <div
                                     key={idx}
+                                    className="exercise-block"
                                     style={{
                                         width: itemWidth+'px',
                                         height: itemHeight+'px',
-                                        display: "inline-block",
                                     }}
                                 >
                                     <div>
