faf07613882852b39772ad64ba6ff855bdd8a18c
Hanson <105476@grundfos.com>
2023-03-29 17:45:36 +0800

    1

--- web/src/components/PoseSync.js
+++ web/src/components/PoseSync.js
@@ -3,27 +3,9 @@ import {
 	distanceBetweenPoints,
 	BlazePoseKeypointsValues,
 	isLowerBodyVisible,
+	pearson_corr,
 } from "./ropes";
 import * as THREE from "three";
-
-const pcorr = (x, y) => {
-	let sumX = 0,
-	  sumY = 0,
-	  sumXY = 0,
-	  sumX2 = 0,
-	  sumY2 = 0;
-	const minLength = x.length = y.length = Math.min(x.length, y.length),
-	  reduce = (xi, idx) => {
-		const yi = y[idx];
-		sumX += xi;
-		sumY += yi;
-		sumXY += xi * yi;
-		sumX2 += xi * xi;
-		sumY2 += yi * yi;
-	  }
-	x.forEach(reduce);
-	return (minLength * sumXY - sumX * sumY) / Math.sqrt((minLength * sumX2 - sumX * sumX) * (minLength * sumY2 - sumY * sumY));
-};
 
 export default class PoseSync {
 	#bufferStepThreshold = 10;
@@ -180,7 +162,7 @@ export default class PoseSync {
 		// console.log(1, d1);
 		// console.log(1, d2);
 
-		console.log(pcorr(d1, d2))
+		console.log(pearson_corr(d1, d2))
 
 		// const ratio = d1[0] / d2[0];
 
